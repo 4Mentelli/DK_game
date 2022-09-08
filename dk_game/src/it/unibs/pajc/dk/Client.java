304f293753ea87599f3b6303c9639952f91b98d2
paoloformentelli <p.formentelli001@studenti.unibs.it>
2022-09-08 16:06:30 +0200

    Client/server start

---
+++ dk_game/src/it/unibs/pajc/dk/Client.java
@@ -0,0 +1,11 @@
+package it.unibs.pajc.dk;
+import java.net.*;
+import java.io.*;
+
+public class Client {
+
+    public static void main(String[] args) throws IOException{
+
+        Socket s = new Socket("localHost", 4999);
+    }
+}
